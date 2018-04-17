c49e5dbb8712cdc3f060c2e94048a2629b4df6e2
OrionPro <orion79@rambler.ru>
2018-04-17 09:53:02 +0300

    Правил уровни готовности на странице purchase_by_the_piece.html 16=7.04.18

--- source/pages/proxi-by-country/proxi-by-country.js
+++ source/pages/proxi-by-country/proxi-by-country.js
@@ -23,7 +23,7 @@ const fillGoalField = function(obj){
 		conPar.parentNode.parentNode.parentNode.classList.add('blur-active');
 		setTimeout(function () {
 			conPar.parentNode.parentNode.parentNode.classList.remove('blur-active');
-		}, 200);
+		}, 500);
 		let content2 = Array.prototype.find.call(conPar.children, item => item.tagName === "SPAN");
 		field.textContent = `${content2.textContent}: ${content1}`;
 		field.value = this.value;
